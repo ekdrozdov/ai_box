28a4c821904bcb0621b2eac9db7a5a715d2fc809
Eduard Drozdov <ekdrozdov@mgmail.com>
2019-01-07 20:01:40 +0700

    add glut in makefile, add graphic test in main

--- makefile
+++ makefile
@@ -1,23 +1,24 @@
 ODIR = obj
 SDIR = src
 BDIR = bin
+LDIR = lib
 OUT = $(BDIR)/matrix.exe
 CC = g++
 INC = -Iinc
-CFLAGS = -std=c++11
+CFLAGS = -std=c++11 -g
+LFLAGS = -mwindows -lopengl32 -lglu32
 
 SOURCES = $(shell find $(SDIR) -name '*.cpp')
 _OBJS = $(patsubst $(SDIR)/%.cpp, %.o, $(SOURCES))
 OBJS = $(patsubst %,$(ODIR)/%,$(_OBJS))
+LIBS = $(shell find $(LDIR) -name '*.lib')
 
 
 $(ODIR)/%.o: $(SDIR)/%.cpp
 	$(CC) -c $(INC) -o $@ $< $(CFLAGS)
 
 $(OUT): $(OBJS)
-	$(CC) -o $(OUT) $(OBJS)
-
-
+	$(CC) -o $(OUT) $(OBJS) $(LIBS) $(LFLAGS)
 
 clean:
 	rm obj/*
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,105 @@
 #include <iostream>
 //#include "world.h"
 #include "tester.h"
-#include "energy_container.h"
-//#include "mesh_2d.h"
+#include "Resource.h"
+#include "mesh_2d.h"
+#include <windows.h>
+#include <GL/glut.h>
+
+bool flag = false;
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(-0.5, 0.5);
+	glVertex2f(0.5, 0.5);
+	glVertex2f(0.5, -0.5);
+	glEnd();
+
+	glFlush();
+}
+
+void key_reaction(unsigned char key, int x, int y)
+{
+	if (flag)
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_POLYGON);
+		glVertex2f(-0.5, -0.5);
+		glVertex2f(-0.5, 0.5);
+		glVertex2f(0.5, 0.5);
+		glVertex2f(0.5, -0.5);
+		glEnd();
+
+		glFlush();
+		flag = false;
+	}
+	else
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_POLYGON);
+		glVertex2f(-0.8, -0.8);
+		glVertex2f(-0.8, 0.8);
+		glVertex2f(0.8, 0.8);
+		glVertex2f(0.8, -0.8);
+		glEnd();
+
+		glFlush();
+		flag = true;
+	}
+}
+
+void init()
+{
+	glClearColor(0.000, 0.110, 0.392, 0.0); // JMU Gold
+
+	glColor3f(0.314, 0.314, 0.000); // JMU Purple
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+}
 
 int main(int argc, char **argv)
 {
-	if (!Tester::run_all_tests())
-	{
-		std::cout << "Some unit tests wasn't passed, exit with failure" << std::endl;
-		return EXIT_FAILURE;
-	}
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(640, 480);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Test");
+	glutDisplayFunc(display);
+	glutKeyboardFunc(key_reaction);
+	init();
+	glutMainLoop();
+	// if (!Tester::run_all_tests())
+	// {
+	// 	return EXIT_FAILURE;
+	// }
 
-	Energy_container ec;
+	//Resource ec;
+	// Mesh_2d mesh = Mesh_2d(Point({10, 10}), Node());
+	// mesh.print_mesh();
+	// std::cout << "total: " << mesh.get_total_energy_value() << std::endl;
+	// std::cout << std::endl;
+
+	// mesh.add_energy_to_node({1, 1}, Energy(9.));
+	// mesh.print_mesh();
+	// std::cout << "total: " << mesh.get_total_energy_value() << std::endl
+	// 		  << std::endl;
+
+	// for (int i = 0; i < 100; ++i)
+	// {
+	// 	mesh.step();
+	// 	mesh.print_mesh();
+	// 	std::cout << "total: " << mesh.get_total_energy_value() << std::endl
+	// 			  << std::endl;
+	// }
+
 	//Mesh_2d_configuration config = Mesh_2d_configuration("mesh_config.txt");
 	//Mesh_2d mesh = Mesh_2d(config);
 
@@ -20,7 +107,7 @@ int main(int argc, char **argv)
 	// Time wTime = Time(1.0);
 	// World world = World(&wTime, &space);
 
-	// world.space->addEnergyToNode(3, 3, 1000.0);
+	// world.space->add_energy_to_node(3, 3, 1000.0);
 
 	// Organism plant = Organism(50.0, 5, 5, &space);
 	// world.plant = &plant;
